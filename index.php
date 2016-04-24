a889abb368427f7eee31944100091c9c416bf6aa
sophson84 <sophiewatson84@gmail.com>
2016-04-24 18:44:39 +0000

    test file

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    
+    mysqli_connect("176.32.230.48", "cl59-example-fc1", "cVYNsyT.w", "cl59-example-fc1");
+    
+    if (mysqli_connect_error()) {
+        echo "Could not connect to database";
+    }
+
+
+?>
